af44d41f7060e663eb0036a43d2240c6558046c1
youxingxiang <1365831278@qq.com>
2020-11-13 15:24:13 +0800

    updateupdate

--- main.go
+++ main.go
@@ -1,17 +1,4 @@
 package main
 
-import (
-	"./examples"
-)
-
 func main() {
-	// 工厂模式例子
-	examples.SimpleFactory()
-
-	// 工厂方法模式（Factory Method）
-	examples.FactoryMethod()
-
-	// 抽象工厂模式（Abstract Factory）
-	examples.AbstractFactory()
-
 }
